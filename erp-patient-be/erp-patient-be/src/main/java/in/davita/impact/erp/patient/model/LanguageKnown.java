cb584749a8ac95f45806c733086515e0164eb185
Chetan Phalke <ChetanP@citiustech.com>
2021-04-07 12:54:02 +0530

    projet struture 1

---
+++ erp-patient-be/erp-patient-be/src/main/java/in/davita/impact/erp/patient/model/LanguageKnown.java
@@ -0,0 +1,25 @@
+package in.davita.impact.erp.patient.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+public class LanguageKnown {
+
+	@Id
+	private int languageId;
+	private String languageName;
+	
+	@ManyToOne
+	private Patient patient;
+	
+	
+}
