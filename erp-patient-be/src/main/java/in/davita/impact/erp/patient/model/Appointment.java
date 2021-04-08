608e3164459e0826be7f076ae4d90379c1a1bbea
VaibhaviB <VaibhaviB@citiustech.com>
2021-04-08 15:22:51 +0530

    Committing changes related to appointment

--- erp-patient-be/src/main/java/in/davita/impact/erp/patient/model/Appointment.java
+++ erp-patient-be/src/main/java/in/davita/impact/erp/patient/model/Appointment.java
@@ -2,11 +2,24 @@ package in.davita.impact.erp.patient.model;
 
 import java.sql.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,29 +30,48 @@ import lombok.Setter;
 public class Appointment {
 	
 	@Id
+	@GenericGenerator(name = "appointment_sequence_id", strategy = "in.davita.impact.erp.patient.utilities.AppointmentIdGenerator")
+	@GeneratedValue(generator = "appointment_sequence_id")
 	private String appointmentId;
 	
 	
-	//@NotBlank(message="PhysicianId is Required")
-	@NotNull(message = "Physician Id is Mandatory")
+	@NotBlank(message="PhysicianId is Required")
+	//@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	//@JoinColumn(name = "credential_id_fk")
 	private String physicianId;
 	
-	//@NotNull
-	//@NotBlank
+	@NotBlank(message="patientId is Required")
 	private String patientId;
 
 	
+	@JsonFormat(pattern="yyyy-MM-dd")
+	@NotNull(message="Appointment date is required")
+	@FutureOrPresent(message="Appointment date must be past or present date") 
+    @Column(nullable = false)
+	 private Date AppointmentDate;
+	
+	@JsonFormat(pattern="yyyy-MM-dd")
+	@FutureOrPresent(message="Appointment date must be past or present date") 
+	private Date reScheduleDate;
+	
 	
 	/*
-	 * @NotNull
+	 * @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="HH:mm:ss")
 	 * 
-	 * @NotBlank
+	 * @Temporal(TemporalType.TIME)
 	 * 
-	 * @FutureOrPresent private Date AppointmentDate;
+	 * @NotNull(message="Appointment Start time is required") private Date
+	 * startTime;
 	 * 
-	 * @FutureOrPresent private Date reScheduleDate;
 	 * 
+	 * @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="HH:mm:ss")
 	 * 
-	 * private String appointmentStatus;
+	 * @Temporal(TemporalType.TIME)
+	 * 
+	 * @NotNull(message="Appointment End time is required") private Date endTime;
 	 */
+	
+	@Pattern(regexp = "Available|Booked")
+	private String appointmentStatus;
+	
 }
