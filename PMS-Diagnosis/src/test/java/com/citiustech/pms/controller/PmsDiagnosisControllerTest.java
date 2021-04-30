38f8a71cd42b4d8333dc4d01b5236f66b132b58c
Pratik Tiwari <PratikT2@citiustech.com>
2021-04-30 18:44:31 +0530

    Added test cases

---
+++ PMS-Diagnosis/src/test/java/com/citiustech/pms/controller/PmsDiagnosisControllerTest.java
@@ -0,0 +1,64 @@
+package com.citiustech.pms.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.citiustech.pms.diagnosis.controller.PmsDiagnosisController;
+import com.citiustech.pms.diagnosis.model.Diagnosis;
+import com.citiustech.pms.diagnosis.model.DiagnosisModel;
+import com.citiustech.pms.diagnosis.model.DiagnosisSuccess;
+import com.citiustech.pms.diagnosis.service.PmsDiagnosisServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class PmsDiagnosisControllerTest {
+
+	@InjectMocks
+	private PmsDiagnosisController pmsDiagnosisController;
+	
+	@Mock
+	private Diagnosis diagnosis;
+	
+	@Mock
+	private PmsDiagnosisServiceImpl pmsDiagnosisServiceImpl;
+	
+	@Mock
+	private DiagnosisModel diagnosisModel;
+	
+	
+	@Test
+	public void GetAllDiagnosis()
+	 {
+		DiagnosisSuccess DiagnosisSuccessExp = new DiagnosisSuccess("abc" , true,null , null);
+		when(pmsDiagnosisServiceImpl.getAllDiagnosis()).thenReturn(DiagnosisSuccessExp);
+		DiagnosisSuccess diagnosisSuccessActual =  pmsDiagnosisController.getAllDiagnosis().getBody();
+		assertEquals(diagnosisSuccessActual.getMessage(), DiagnosisSuccessExp.getMessage());
+	 }
+	
+	@Test
+	public void getDiagnosisDetailDesc()
+	 {
+		DiagnosisSuccess DiagnosisSuccess = new DiagnosisSuccess();
+		DiagnosisSuccess.setMessage("Added Successfully");
+		
+		when(pmsDiagnosisServiceImpl.getDiagnosisDescription(diagnosisModel)).thenReturn(DiagnosisSuccess);
+		DiagnosisSuccess diagnosisSuccessActual =  pmsDiagnosisController.getDiagnosisDetailDesc(diagnosisModel).getBody();
+		assertEquals("Added Successfully", diagnosisSuccessActual.getMessage());
+	 }
+	
+	@Test
+	public void getDiagnosisByVisitId()
+	 {
+		String str ="A001";
+		DiagnosisSuccess DiagnosisSuccessExp = new DiagnosisSuccess();
+		
+		when(pmsDiagnosisServiceImpl.getProcedureByVisitId(str)).thenReturn(DiagnosisSuccessExp);
+		DiagnosisSuccess diagnosisSuccessActual =  pmsDiagnosisController.getProcedureByVisitId(str).getBody();
+		assertEquals(DiagnosisSuccessExp, diagnosisSuccessActual);
+	 }
+}
---
+++ PMS-Diagnosis/src/test/java/com/citiustech/pms/service/PmsDiagnosisServiceImplTest.java
@@ -0,0 +1,151 @@
+package com.citiustech.pms.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.citiustech.pms.diagnosis.exception.DiagnosisException;
+import com.citiustech.pms.diagnosis.model.Diagnosis;
+import com.citiustech.pms.diagnosis.model.DiagnosisDetails;
+import com.citiustech.pms.diagnosis.model.DiagnosisMaster;
+import com.citiustech.pms.diagnosis.model.DiagnosisModel;
+import com.citiustech.pms.diagnosis.model.DiagnosisSuccess;
+import com.citiustech.pms.diagnosis.repository.DiagnosisMasterRepo;
+import com.citiustech.pms.diagnosis.repository.DiagnosisRepo;
+import com.citiustech.pms.diagnosis.service.PmsDiagnosisServiceImpl;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PmsDiagnosisServiceImplTest {
+
+	@InjectMocks
+	private PmsDiagnosisServiceImpl pmsDiagnosisServiceImpl;
+	
+	@Mock
+	private DiagnosisRepo diagnosisRepo;
+	
+	@Mock
+	private DiagnosisMasterRepo diagnosisMasteRepo;
+	
+	@Mock
+	private Diagnosis diagnosis;
+	
+	@Test
+	public void retrieveAllItemsBasic() {
+		DiagnosisMaster diagnosisMaster = new DiagnosisMaster();
+		diagnosisMaster.setDiagnosisId("A1814");
+		diagnosisMaster.setDiagnosisName("Tuberculosis of prostate");
+		
+		List<DiagnosisMaster> list = Arrays.asList(diagnosisMaster);
+		doReturn(list).when(diagnosisMasteRepo).findAll();
+		assertEquals(1,pmsDiagnosisServiceImpl.getAllDiagnosis().getDiagnosisMaster().size());
+	}
+	
+	@Test
+	public void getAllDiagnosisException() {
+		DiagnosisMaster diagnosisMaster = new DiagnosisMaster();
+		diagnosisMaster.setDiagnosisId("");
+		
+		List<DiagnosisMaster> list = new ArrayList<DiagnosisMaster>();
+		list.add(diagnosisMaster);
+		
+		Assertions.assertThrows(DiagnosisException.class, () -> {
+			pmsDiagnosisServiceImpl.getAllDiagnosis();
+		  });
+	}
+	
+	@Test
+	public void getDiagnosisDescription() {
+		
+		DiagnosisModel diagnosisModel = new DiagnosisModel();
+		diagnosisModel.setPatient_visit_id("P101");
+		
+		List<DiagnosisDetails> diagnosisDetailslist = new ArrayList<DiagnosisDetails>();
+		DiagnosisDetails diagnosisDetails = new DiagnosisDetails();
+		diagnosisDetails.setId("P101");
+		diagnosisDetailslist.add(diagnosisDetails);
+		diagnosisModel.setDiagnosis_details(diagnosisDetailslist);
+		
+		boolean actual = pmsDiagnosisServiceImpl.getDiagnosisDescription(diagnosisModel).isSuccessFlag();
+		
+		assertEquals(Boolean.TRUE, actual);
+	}
+	
+	@Test
+	public void getDiagnosisDescriptionPatientVisitID() {
+		
+		DiagnosisModel diagnosisModel = new DiagnosisModel();
+		diagnosisModel.setPatient_visit_id("");
+		
+		Assertions.assertThrows(DiagnosisException.class, () -> {
+			pmsDiagnosisServiceImpl.getDiagnosisDescription(diagnosisModel);
+		  });
+	}
+	
+	@Test
+	public void getDiagnosisDescriptionException() {
+		
+		DiagnosisModel diagnosisModel = new DiagnosisModel();
+		diagnosisModel.setPatient_visit_id("P101");
+		List<DiagnosisDetails> diagnosisList = new ArrayList<DiagnosisDetails>();
+		diagnosisModel.setDiagnosis_details(diagnosisList);
+		
+		Assertions.assertThrows(DiagnosisException.class, () -> {
+			pmsDiagnosisServiceImpl.getDiagnosisDescription(diagnosisModel);
+		  });
+	}
+	
+	@Test
+	public void getDiagnosisByVisitId() {
+		
+		String str ="P101";
+		List<Diagnosis> getPatientVisitId = new ArrayList<>();
+		Diagnosis diagnosis = new Diagnosis();
+		diagnosis.setPatient_visit_id("P101");
+		diagnosis.setName("ABC");
+		getPatientVisitId.add(diagnosis);
+		
+		DiagnosisSuccess diagnosisExp = new DiagnosisSuccess();
+		diagnosisExp.setSuccessFlag(Boolean.TRUE);
+		diagnosisExp.setDiagnosis(getPatientVisitId);
+		
+		when(diagnosisRepo.checkForExistingPatientVisitId(str)).thenReturn(getPatientVisitId);
+
+		DiagnosisSuccess ProcedureActual = pmsDiagnosisServiceImpl.getProcedureByVisitId(str);
+
+		assertEquals(diagnosisExp, ProcedureActual);
+	}
+	
+	@Test
+	public void getDiagnosisByVisitIdCheckIsEmpty() {
+		
+		String str = "";
+		
+		pmsDiagnosisServiceImpl.getProcedureByVisitId(str);
+	}
+	
+	@Test
+	public void getDiagnosisByVisitIdException() {
+		
+		String str = "P101";
+		List<Diagnosis> getPatientVisitId = new ArrayList<>();
+		Diagnosis diagnosis = new Diagnosis();
+		diagnosis.setPatient_visit_id(null);
+		getPatientVisitId.add(diagnosis);
+		
+		Assertions.assertThrows(DiagnosisException.class, () -> {
+			pmsDiagnosisServiceImpl.getProcedureByVisitId(str);
+		  });
+	}
+}
