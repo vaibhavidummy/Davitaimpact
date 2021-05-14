b0cb652e514fc9c6bae814ccf7e1dc0665bc124f
pranavekapure <pranav.ekapure@citiustech.com>
2021-05-14 22:57:38 +0530

    MedicationApplicationPranav || added eureka client annotation

--- MedicationApplication/src/main/java/com/medication/MedicationApplication.java
+++ MedicationApplication/src/main/java/com/medication/MedicationApplication.java
@@ -2,8 +2,10 @@ package com.medication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.cassandra.config.EnableCassandraAuditing;
 
+@EnableDiscoveryClient
 @EnableCassandraAuditing
 @SpringBootApplication
 public class MedicationApplication {
