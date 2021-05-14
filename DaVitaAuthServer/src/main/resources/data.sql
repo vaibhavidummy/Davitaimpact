13952e3f7d78e612697234f4d6b135faf7348a40
Prashant Wankhede <PrashantW3@citiustech.com>
2021-05-14 19:38:33 +0530

    added data.sql

---
+++ DaVitaAuthServer/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `davitademo`.`oauth_access_token` 
+CHANGE COLUMN `authentication` `authentication` LONGBLOB NULL DEFAULT NULL ,
+CHANGE COLUMN `token` `token` LONGBLOB NULL DEFAULT NULL ;
+
+ALTER TABLE `davitademo`.`oauth_client_token` 
+CHANGE COLUMN `token` `token` LONGBLOB NULL DEFAULT NULL ;
+
+ALTER TABLE `davitademo`.`oauth_code` 
+CHANGE COLUMN `authentication` `authentication` LONGBLOB NULL DEFAULT NULL
+
+
+ALTER TABLE `davitademo`.`oauth_refresh_token` 
+CHANGE COLUMN `authentication` `authentication` LONGBLOB NULL DEFAULT NULL ,
+CHANGE COLUMN `token` `token` LONGBLOB NULL DEFAULT NULL ;
+
+INSERT INTO `` (`client_id`,`access_token_validity`,`additional_information`,`authorities`,`authorized_grant_types`,`autoapprove`,`client_secret`,`refresh_token_validity`,`resource_ids`,`scope`,`web_server_redirect_uri`) VALUES ('kidclient',2900,NULL,'ROLE_CLIENT','password,refresh_token',NULL,'{bcrypt}$2a$10$cByTPhGb1C6rGTpxUJCLAOsIuH9f.3LfyBIHnNCFo2f1pACfE07xq',2592000,'ehealth','read,write,trust',NULL);
