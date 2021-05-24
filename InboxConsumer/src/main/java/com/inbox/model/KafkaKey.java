37b03e14b2174697abab00d624721a9b380dbdca
pranavekapure <pranav.ekapure@citiustech.com>
2021-05-24 17:13:54 +0530

    inboxApplication | application name finalised

--- InboxConsumer/src/main/java/com/inbox/model/KafkaKey.java
+++ InboxConsumer/src/main/java/com/inbox/model/KafkaKey.java
@@ -1,6 +1,7 @@
 package com.inbox.model;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.cassandra.core.mapping.Column;
@@ -8,21 +9,27 @@ import org.springframework.data.cassandra.core.mapping.Embedded;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonInclude(Include.NON_NULL)
 @Table("records")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class KafkaKey {
 
+	@PrimaryKey(value = "record_id")
+	private String recordId = UUID.randomUUID().toString();
 	@Column("from_id")
 	private String fromId;
 	@Column("from_name")
 	private String fromName;
-	@PrimaryKey(value = "to_id")
+	@Column("to_id")
 	private String toId;
 	@Column("to_name")
 	private String toName;
