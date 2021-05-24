5dea8b682128290e277d16b0defb8c1c9f25e9e9
pranavekapure <pranav.ekapure@citiustech.com>
2021-05-24 19:03:01 +0530

    authorization added

--- MedicationApplication/src/main/java/com/medication/MedicationApplication.java
+++ MedicationApplication/src/main/java/com/medication/MedicationApplication.java
@@ -2,9 +2,10 @@ package com.medication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.cassandra.config.EnableCassandraAuditing;
-
+@EnableOAuth2Sso
 @EnableDiscoveryClient
 @EnableCassandraAuditing
 @SpringBootApplication
