a6b756f8ab21bc5bf0bcfda7c29cdc39224c6bfc
pranavekapure <pranav.ekapure@citiustech.com>
2021-05-25 15:52:17 +0530

    removed cors as already present in authorization

--- MedicationApplication/src/main/java/com/medication/controller/MedicationController.java
+++ MedicationApplication/src/main/java/com/medication/controller/MedicationController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,6 @@ import com.medication.model.MedicationDto;
 import com.medication.model.MedicationOnVisit;
 import com.medication.service.impl.MedicationServiceImpl;
 
-@CrossOrigin(origins="*",  allowedHeaders="*")
 @RequestMapping(value = "/medication")
 @RestController
 public class MedicationController {
