32c48b638cf5e1a4ffb642876e32f4c08339ac31
pranavekapure <pranav.ekapure@citiustech.com>
2021-05-26 16:57:25 +0530

    inboxApplication | added authorization

--- InboxConsumer/src/main/java/com/inbox/InboxConsumerApplication.java
+++ InboxConsumer/src/main/java/com/inbox/InboxConsumerApplication.java
@@ -2,8 +2,12 @@ package com.inbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 @EnableKafka
+@EnableOAuth2Sso
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InboxConsumerApplication {
 
--- InboxProducer/src/main/java/com/inbox/InboxProducerApplication.java
+++ InboxProducer/src/main/java/com/inbox/InboxProducerApplication.java
@@ -2,7 +2,10 @@ package com.inbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableOAuth2Sso
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InboxProducerApplication {
 
