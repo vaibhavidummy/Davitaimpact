f58b970aa1e5d48cf34bab117a5c1df9fb2ecfe5
Pratik Tiwari <PratikT2@citiustech.com>
2021-06-11 21:40:29 +0530

    Removed unwanted code

--- PMS-Diagnosis/src/main/java/com/citiustech/pms/diagnosis/repository/DiagnosisRepo.java
+++ PMS-Diagnosis/src/main/java/com/citiustech/pms/diagnosis/repository/DiagnosisRepo.java
@@ -3,7 +3,6 @@ package com.citiustech.pms.diagnosis.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,8 +11,5 @@ import com.citiustech.pms.diagnosis.model.Diagnosis;
 @Repository
 public interface DiagnosisRepo extends JpaRepository<Diagnosis, Long> {
 
-	@Query(value="SELECT * FROM diagnosis WHERE patient_visit_id=:patientVisitId", nativeQuery = true )
-	List<Diagnosis> checkForExistingPatientVisitId(@Param("patientVisitId") String patientVisitId);
-	
 	List<Diagnosis> findByPatientVisitId(@Param("patientVisitId") String patientVisitId);
 }
--- PMS-Procedure/src/main/java/com/citiustech/pms/procedure/repository/ProdecureDetailRepository.java
+++ PMS-Procedure/src/main/java/com/citiustech/pms/procedure/repository/ProdecureDetailRepository.java
@@ -3,7 +3,6 @@ package com.citiustech.pms.procedure.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +12,5 @@ import com.citiustech.pms.procedure.model.ProcedureMain;
 @Repository
 public interface ProdecureDetailRepository extends JpaRepository<ProcedureMain, Long> {
 
-	@Query(value="SELECT * FROM procedure_main  WHERE patient_visit_id=:patientVisitId", nativeQuery = true )
-	List<ProcedureMain> checkForExistingPatientVisitId(@Param("patientVisitId") String patientVisitId);
-	
 	List<ProcedureMain> findByPatientVisitId(@Param("patientVisitId") String patientVisitId);
 }
